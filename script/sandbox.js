cebee363aca273b9d0bc3a7f6f9ab742b1764f49
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-05-25 11:46:13 +0200

    first commit

---
+++ script/sandbox.js
@@ -0,0 +1,16 @@
+var names = ["mohamed", 'ahmed', "mostafa"];
+names[0] = "2";
+var numbers = [2, "ahmed"];
+numbers = [21];
+// object
+var mohamed = {
+    name: "mohamed",
+    title: "front end developer",
+    age: 20
+};
+mohamed.age = 30;
+mohamed = {
+    name: "omar",
+    title: "bro max",
+    age: 20
+};
---
+++ script/sandbox.ts
@@ -0,0 +1,24 @@
+let names = ["mohamed", 'ahmed', "mostafa"];
+
+names[0] = "2"
+
+let numbers = [2, "ahmed"]
+
+numbers = [21]
+
+// object
+
+let mohamed = {
+    name: "mohamed",
+    title: "front end developer",
+    age: 20,
+
+}
+
+mohamed.age = 30
+
+mohamed = {
+    name: "omar",
+    title:"bro max",
+    age: 20,
+}
