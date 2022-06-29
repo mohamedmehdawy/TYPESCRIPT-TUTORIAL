0cd2836ac83603469caaa0c0853daeebd974eb1a
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-06-29 07:23:53 +0200

    learn modules

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,13 @@
-// classes
-class Invoice {
-    constructor(
-        readonly client:string,
-        private details: string,
-        public amount: number,
-    ) {}
-
-    format() {
-        return `${this.client} and have ${this.amount}$, ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice.js";
 
 const invoceOne: Invoice = new Invoice("mohamed", "from egypt", 200);
 
 const invoceTwo: Invoice = new Invoice("elsayed", "from egypt", 500);
-console.log(invoceOne.client)
+console.log(invoceOne.client);
 const invoces: Invoice[] = [];
-invoces.push(invoceOne); 
-invoces.push(invoceTwo); 
+invoces.push(invoceOne);
+invoces.push(invoceTwo);
 
-invoces.forEach(ele => {
-    console.log(ele.format())
-})
+invoces.forEach((ele) => {
+    console.log(ele.format());
+});
